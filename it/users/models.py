33b0b74ec47c243ea450e7929de2d778d01ec718
zehregit <zetdcgithub@gmail.com>
2024-01-22 14:29:37 +0200

    presentation changes

--- it/users/models.py
+++ it/users/models.py
@@ -70,7 +70,7 @@ class Designations(models.Model):
     chk = models.CharField(max_length=100, blank=True)
     
     def __str__(self):
-        return self.role
+        return self.identifier
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -80,5 +80,3 @@ class Notification(models.Model):
     url = models.CharField( max_length=250)
     def __str__(self):
         return self.message
-
-        return self.description
