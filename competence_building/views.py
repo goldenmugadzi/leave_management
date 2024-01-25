33d017c874fba4c92fe03bbc3f4977f8952a4c83
ruvheneko <ruvheneco@gmail.com>
2024-01-25 16:19:34 +0200

    regional charts

--- competence_building/views.py
+++ competence_building/views.py
@@ -18,6 +18,20 @@ def view_regionaloffice(request):
 def view_jobdescription(request):
         return render(request, 'competence_building/jobdescription.html')
 
+def view_IT(request):
+        return render(request, 'competence_building/IT.html')
 
-
+def view_finance(request):
+        return render(request, 'competence_building/finance.html')
    
+def view_humanresource(request):
+        return render(request, 'competence_building/humanresource.html')
+
+def view_losscontrol(request):
+        return render(request, 'competence_building/losscontrol.html')
+
+def view_engineering(request):
+        return render(request, 'competence_building/engineering.html')
+
+def view_commercial(request):
+        return render(request, 'competence_building/commercial.html')
