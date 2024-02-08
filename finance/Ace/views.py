072449a5a9b8f3410b3df80db4a5e3ba5b753c12
ashley <ashleyzarter@gmsil.com>
2024-02-08 15:23:09 +0200

    requirements

--- finance/Ace/views.py
+++ finance/Ace/views.py
@@ -716,7 +716,7 @@ def get_Ace_records_requester(request):
     records = Ace.objects.filter(requested_by=requested_by).all()
     context = serializers.serialize('json', records)
 
-    user_page = 'Ace/index_requester.html'
+    user_page = 'ace/index_requester.html'
     # print(context)
 
     return render(request, user_page, {"title": "All Records",
