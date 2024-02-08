727f9a66d4532e0a63c7b048aed1b0988cab4d76
ashley <ashleyzarter@gmsil.com>
2024-02-08 09:26:50 +0200

    requirements

--- beii_v1/settings.py
+++ beii_v1/settings.py
@@ -190,20 +190,20 @@ DATABASES = {
     #     'PASSWORD': 'Z3tdc_it@2021_',
     #     'HOST': 'localhost'
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'beii',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'beii',
-    #     'USER': 'excellence',
-    #     'PASSWORD': 'Z3tdc_it@2021_',
-    #     'HOST': 'localhost'
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'beii',
+        'USER': 'excellence',
+        'PASSWORD': 'Z3tdc_it@2021_',
+        'HOST': 'localhost'
+    }
     # Localhost
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
