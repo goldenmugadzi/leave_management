bcf6fb85ef96ac4b58e6215bf3ec58ceafe485ac
zehregit <zetdcgithub@gmail.com>
2024-02-16 13:40:07 +0200

    ...

--- beii_v1/settings.py
+++ beii_v1/settings.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'rest_framework',
     'rest_framework_simplejwt',
+    
     'risk.audit.checklist',
     'risk.audit.nonconformity',
     'it.beii_auth.apps.BeiiAuthConfig',
