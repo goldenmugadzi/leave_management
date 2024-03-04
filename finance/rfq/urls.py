f28ee9ff54afb1eb87dea7cd869b062f00a420e9
zehregit <zetdcgithub@gmail.com>
2024-03-04 14:38:49 +0200

    rfq pull

--- finance/rfq/urls.py
+++ finance/rfq/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create_rfq_from_ace', views.create_rfq_from_ace, name='create_rfq'),
-    path('approve_rfq', views.get_to_approve_rfq, name='approve_rfq'),
-    path('reject_rfq', views.get_to_reject_rfq, name='reject_rfq'),
     path('', index, name='index'),
     path('rfq_create', rfq_create, name='create_rfq'),
     path('create_rfq', create_rfq, name='create_rfq'),
