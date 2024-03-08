d60e35b4ef150a1964f01cb6c9093d15f6d8ac73
zehregit <zetdcgithub@gmail.com>
2024-03-08 09:09:14 +0200

    ...

--- finance/rfq/urls.py
+++ finance/rfq/urls.py
@@ -4,8 +4,8 @@ from .views import create_rfq_from_ace, get_to_approve_rfq, get_to_reject_rfq, i
 
 urlpatterns = [
     path('', index, name='index'),
-    path('rfq_create', rfq_create, name='create_rfq'),
-    path('create_rfq', create_rfq, name='create_rfq'),
+    # path('rfq_create', rfq_create, name='create_rfq'),
+    # path('create_rfq', create_rfq, name='create_rfq'),
     path('create_ace_rfq', create_rfq_from_ace, name='create_rfq'),
     path('approve_rfq', get_to_approve_rfq, name='approve_rfq'),
     path('reject_rfq', get_to_reject_rfq, name='reject_rfq'),
