aa02a314e780815b9e56091d34518fe4ebb8026b
zehregit <zetdcgithub@gmail.com>
2024-03-08 14:16:47 +0200

    ...

--- beii_v1/settings.py
+++ beii_v1/settings.py
@@ -193,7 +193,7 @@ DATABASES = {
     # }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'beii',
+    #     'NAME': 'beii_test',
     #     'USER': 'root',
     #     'PASSWORD': '',
     #     'HOST': 'localhost',
