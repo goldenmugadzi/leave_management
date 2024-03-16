2d4cb3238ac76fd6bb6eda52fd619093186f186a
zehregit <zetdcgithub@gmail.com>
2024-03-16 17:08:58 +0200

    ...

--- beii_v1/settings.py
+++ beii_v1/settings.py
@@ -177,13 +177,13 @@ WSGI_APPLICATION = 'beii_v1.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'beii',
-        'USER': 'beii_user',
-        'PASSWORD': 'v4mnwVrOsAupSY0k',
-        'HOST': '172.16.8.22'
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'beii',
+    #     'USER': 'beii_user',
+    #     'PASSWORD': 'v4mnwVrOsAupSY0k',
+    #     'HOST': '172.16.8.22'
+    # }
     # Localhost
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -198,6 +198,20 @@ DATABASES = {
     #     'USER': 'root',
     #     'PASSWORD': '',
     #     'HOST': 'localhost',
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'beii_vi',
+        'USER': 'be_user',
+        'PASSWORD': 'Z3tdc_it@2024',
+        'HOST': 'localhost'
+    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'beii',
+    #     'USER': 'excellence',
+    #     'PASSWORD': 'Z3tdc_it@2021_',
+    #     'HOST': 'localhost'
     # }
     # Localhost
     # 'default': {
