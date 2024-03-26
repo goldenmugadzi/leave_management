6407b2254a235f67b46ec20ccc066ca3ac132b86
ashley mugwambi <70527457+ashiemugwa1997@users.noreply.github.com>
2024-03-26 11:35:04 +0200

    create pettycash

--- finance/PettyCash/apps.py
+++ finance/PettyCash/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PettycashConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Pettycash'
+    name = 'PettyCash'
--- finance/Pettycash/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Pettycash
-
-admin.site.register(Pettycash)
