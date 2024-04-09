aa0c8fe1e887492a4572c8186d9386663b8ffa64
vaChinaka <perseychinaka@gmail.com>
2024-04-09 10:51:04 +0200

    prprepare to merg tempertoken

--- finance/purchase_request/models.py
+++ finance/purchase_request/models.py
@@ -4,8 +4,6 @@ from django.db import models
 from approve.models import Process
 from finance.Ace.models import Ace
 from it.users.models import UserProfile, Sections, Roles, Supplier
-
-
 class PurchaseRequest(models.Model):
     id = models.CharField(primary_key=True, max_length=20, editable=False)
     description = models.TextField(blank=True, null=True)
@@ -20,29 +18,22 @@ class PurchaseRequest(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     section = models.ForeignKey(Sections, on_delete=models.CASCADE)
     ace = models.ForeignKey(Ace, on_delete=models.SET_NULL, blank=True, null=True)
-    process = models.OneToOneField(Process, on_delete=models.SET_NULL, blank=True, null=True)
-
+    process = models.ForeignKey(Process, on_delete=models.SET_NULL, blank=True, null=True)
     def __str__(self):
         return self.description
-
     def save(self, *args, **kwargs):
         timestamp = str(int(time.time()))
         random_number = str(random.randint(10000, 99999))
         self.id = "PR" + timestamp + random_number
         super().save(*args, **kwargs)
-
-
 class Quotation(models.Model):
     purchase_request = models.ForeignKey(PurchaseRequest, on_delete=models.CASCADE,blank=True, null=True)
     quotation_file = models.FileField(upload_to='uploads/purchase_request')
     supplier = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     created_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE,blank=True, null=True)   
-
-
     def __str__(self):
-        return f"Quotation from {self.supplier} for Order {self.purchase_request}"
-
+        return f'{self.purchase_request} - {self.supplier}'
 class Item(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
     description = models.CharField(max_length=100, blank=True, null=True)
@@ -50,55 +41,36 @@ class Item(models.Model):
     price = models.FloatField(blank=True, null=True)
     quantity = models.IntegerField(blank=True, null=True)
     quotation = models.ForeignKey(Quotation, models.DO_NOTHING, blank=True, null=True)
-    class Meta:
-        ordering = ['name']
-
     def __str__(self):
-        return self.name
-
-
-
+        return str(self.pk)
 class Bid(models.Model):
     purchase_request = models.ForeignKey(PurchaseRequest, on_delete=models.CASCADE)
     response_received = models.BooleanField(default=False)
     second_non_response = models.BooleanField(default=False)
     Supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
-
     def __str__(self):
         return str(self.pk)
-
-        return f"Bid for Purchase Request {self.purchase_request}"
-
 class Order(models.Model):
     purchase_request = models.ForeignKey(PurchaseRequest, on_delete=models.CASCADE)
     bid = models.ForeignKey(Bid, on_delete=models.CASCADE)
     signed = models.BooleanField(default=False)
     cancelled = models.BooleanField(default=False)
-
     def __str__(self):
-        return self.order_number
-    
+        return str(self.pk)
     def save(self, *args, **kwargs):
         timestamp = str(int(time.time()))
         random_number = str(random.randint(10000, 99999))
         self.id = "odr" + timestamp + random_number
         super().save(*args, **kwargs)
-
-
-
 class SiteVisit(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     comment = models.TextField(blank=True, null=True)
     scheduled = models.BooleanField(default=False)
     completed = models.BooleanField(default=False)
-
     def __str__(self):
-        return f"Site Visit for Order {self.order}"
-
-
+        return str(self.pk)
 class Delivery(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     able_to_deliver = models.BooleanField(default=False)
-
     def __str__(self):
-        return f"Delivery for Order {self.order}"
+        return str(self.pk)
--- finance/purchase_request/views.py
+++ finance/purchase_request/views.py
@@ -136,21 +136,22 @@ def view_all_purchase_requests(request):
 @login_required
 def quote_purchase_request(request, purchase_request_id):
     purchase_request = PurchaseRequest.objects.get(id=purchase_request_id)
-    count = int(request.GET.get('items', 5))  # Default value of 5 if no 'extra' parameter is provided
-    items = inlineformset_factory( Quotation,Item, form=ItemForm, extra=count, can_delete=False)
-    
-    if request.method == 'POST':
-        form = QuotationForm(request.POST, request.FILES)
-        formset = items(request.POST, instance=purchase_request)
-        instances = formset.save(commit=False)
+    ItemFormSet = inlineformset_factory(Quotation, Item, form=ItemForm, extra=int(request.POST.get('items', 5)), can_delete=False)
+
+    if request.method == 'POST' and not request.POST.get('get'):
+        form = QuotationForm(request.POST, request.FILES, instance=purchase_request)
         if form.is_valid():
             quotation = form.save(commit=False)
             quotation.purchase_request = purchase_request
             quotation.created_by = request.user
             quotation.save()
-            for instance in instances:
-                instance.quotation = quotation
-                instance.save()
+            formset = ItemFormSet(request.POST, instance=quotation)
+            if formset.is_valid():
+                formset.save()
+
             return redirect('purchase_request:purchase_request_detail', purchase_request_id)
-        
-    return render(request, 'finance/purchase_request/create_quote.html', {'formset': items,'quote': QuotationForm()})
+    else:
+        form = QuotationForm()
+        formset = ItemFormSet(instance=Quotation())
+
+    return render(request, 'finance/purchase_request/create_quote.html', {'formset': formset, 'quote': form})
