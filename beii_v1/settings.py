89bf409b3789f3d028d299b3ecbc685a6bd67842
ashley mugwambi <70527457+ashiemugwa1997@users.noreply.github.com>
2024-04-28 15:18:55 +0200

    pettycash clearing

--- beii_v1/settings.py
+++ beii_v1/settings.py
@@ -193,7 +193,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'beii_new',
+        'NAME': 'beii_test',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
