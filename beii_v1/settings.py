c66114604bc6f964cb8c3e7868c6800cada7bb7c
zehregit <zetdcgithub@gmail.com>
2024-04-29 10:30:18 +0200

    potential conflicts

--- beii_v1/settings.py
+++ beii_v1/settings.py
@@ -195,7 +195,7 @@ DATABASES = {
             'sql_mode': 'NO_BACKSLASH_ESCAPES',
         },
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'beii_test',
+        'NAME': 'beii_new',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
