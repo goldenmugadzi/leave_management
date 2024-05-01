7cebf682b5dfbbe744fd6a4918bf0811ed35782b
vaChinaka <perseychinaka@gmail.com>
2024-05-01 12:52:50 +0200

    asdf

--- finance/purchase_request/views.py
+++ finance/purchase_request/views.py
@@ -261,7 +261,7 @@ def uploaduuom(request):
                 # 'ace': item['ace'],
                 'scope_of_work': item['scope_of_work'],
             }
-        purchase_request, created = PurchaseRequest.objects.get_or_create(id=item['rfq_number'], defaults=defaults)
+        purchase_request, created = PurchaseRequest.objects.get_or_create(pr_no=item['rfq_number'], defaults=defaults)
         print(item,"\n\n\n\n\n\n\npritem\n\n", item['uom'])
         try:pritem = PrItem(item_required=item['item_required'],
                         unit_of_measurement = UnitOfMeasurement.objects.get(Q(unit__iexact=item['uom']) | Q(name__iexact=item['uom'])),
