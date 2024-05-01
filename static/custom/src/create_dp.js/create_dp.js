01d12b836702f1f003e7ac72d66e62a90de08fa4
zehregit <zetdcgithub@gmail.com>
2024-05-01 16:18:28 +0200

    ...

--- static/custom/src/create_dp.js/create_dp.js
+++ static/custom/src/create_dp.js/create_dp.js
@@ -15,8 +15,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var e = React.createElement;
-var BASE_URL = "http://localhost:8000/direct_purchase";
-// const BASE_URL = "http://172.16.8.98:9300";
+// const BASE_URL = "http://localhost:8000/direct_purchase";
+var BASE_URL = "http://172.16.8.98:9300/direct_purchase";
 
 var CreateDP = function (_React$Component) {
   _inherits(CreateDP, _React$Component);
