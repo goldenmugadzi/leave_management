876782e65f88d9f86300a69d36c0a89e612d15a8
zehregit <zetdcgithub@gmail.com>
2024-05-05 19:51:44 +0200

    ...

--- approve/views.py
+++ approve/views.py
@@ -126,10 +126,11 @@ def approve_step(request, process_id):
                 if process.workflow.name == 'tokens':
                     return False #redirect('tokens:token', process.token_set.last().id)
                 elif process.workflow.name == 'pettycash':
-                    return redirect('pettycash:pettycash_detail', process.pettycash_set.last().id)
+                    return redirect('pettycash:pettycash_detail', process.pettycash_set.last().petty_id)
 
                 else:
                     return redirect('approve:workflow_detail', process.workflow.id)
+                
         messages.error(request, 'You are not allowed to approve')
         if process.workflow.name == 'purchase request':
             return redirect('purchase_request:purchase_request_detail', process.purchaserequest_set.last().id)
@@ -137,7 +138,7 @@ def approve_step(request, process_id):
         if process.workflow.name == 'tokens':
             return redirect('tokens:token', process.token_set.last().id)
         elif process.workflow.name == 'pettycash':
-            return redirect('pettycash:pettycash_detail', process.pettycash_set.last().id)
+            return redirect('pettycash:pettycash_detail', process.pettycash_set.last().petty_id)
 
         else:
             return redirect('approve:workflow_detail', process.workflow.id)
@@ -149,7 +150,7 @@ def approve_step(request, process_id):
     if process.workflow.name == 'tokens':
         return redirect('tokens:token', process.token_set.last().id)
     elif process.workflow.name == 'pettycash':
-        return redirect('pettycash:pettycash_detail', process.pettycash_set.last().id)
+        return redirect('pettycash:pettycash_detail', process.pettycash_set.last().petty_id)
 
     else:
         return redirect('approve:workflow_detail', process.workflow.id)
