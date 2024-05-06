58cf29b91c6a02f5810101c05d6ae93eb2797e31
zehregit <zetdcgithub@gmail.com>
2024-05-06 12:29:47 +0200

    ...

--- beii_v1/settings.py
+++ beii_v1/settings.py
@@ -41,10 +41,12 @@ ALLOWED_HOSTS = ['localhost', '172.16.8.97', '172.16.8.22', '127.0.0.1', '172.16
 #     "http://localhost:3000",
 #     "http://172.16.8.99:9300"
 # ]
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_TRUSTED_ORIGINS = ['http://172.16.8.97', "http://172.16.8.97:3200", "http://172.16.8.99",
-                        "http://172.16.8.99:9300", "http://172.16.8.98", "http://172.16.8.98:9300"]
+                        "http://172.16.8.99:9300", "http://172.16.8.98", "http://172.16.8.98:9300",
+                        "http://bexcel.zedc.co.zw:9300"]
 
 CORS_ALLOW_HEADERS = ('content-disposition', 'accept-encoding',
                       'content-type', 'accept', 'origin', 'authorization')
