01a13d06f873c10f0c5c9754b8e8311d06d408d4
zehregit <zetdcgithub@gmail.com>
2024-05-08 19:10:06 +0200

    ...

--- finance/comparative_schedules/views.py
+++ finance/comparative_schedules/views.py
@@ -227,7 +227,7 @@ def get_comperative_schedules(request):
         print("role id:", role.id)
         user_ace_role_ = Roles.objects.filter(id=role.id).first() if role.id else None
         print("role application:", user_ace_role_.application)
-        if user_ace_role_.application == "comparative_schedules":
+        if user_ace_role_.application == "comparative_schedule":
             if user_ace_role_.role == "check":
                 fm_role = True
             if user_ace_role_.role == "approve":
@@ -249,7 +249,7 @@ def get_all_schedules(request):
         print("role id:", role.id)
         user_ace_role_ = Roles.objects.filter(id=role.id).first() if role.id else None
         print("role application:", user_ace_role_.application)
-        if user_ace_role_.application == "comparative_schedules":
+        if user_ace_role_.application == "comparative_schedule":
             if user_ace_role_.role == "check":
                 fm_role = True
             if user_ace_role_.role == "approve":
@@ -424,7 +424,7 @@ def get_pending_committee(request):
         print("role id:", role.id)
         user_ace_role_ = Roles.objects.filter(id=role.id).first() if role.id else None
         print("role application:", user_ace_role_.application)
-        if user_ace_role_.application == "comparative_schedules":
+        if user_ace_role_.application == "comparative_schedule":
             if user_ace_role_.role == "check":
                 fm_role = True
             if user_ace_role_.role == "approve":
@@ -546,7 +546,7 @@ def get_pending_gm_approval(request):
         print("role id:", role.id)
         user_ace_role_ = Roles.objects.filter(id=role.id).first() if role.id else None
         print("role application:", user_ace_role_.application)
-        if user_ace_role_.application == "comparative_schedules":
+        if user_ace_role_.application == "comparative_schedule":
             if user_ace_role_.role == "check":
                 fm_role = True
             if user_ace_role_.role == "approve":
@@ -666,7 +666,7 @@ def get_pending_fm_approval(request):
         print("role id:", role.id)
         user_ace_role_ = Roles.objects.filter(id=role.id).first() if role.id else None
         print("role application:", user_ace_role_.application)
-        if user_ace_role_.application == "comparative_schedules":
+        if user_ace_role_.application == "comparative_schedule":
             if user_ace_role_.role == "check":
                 fm_role = True
             if user_ace_role_.role == "approve":
@@ -703,7 +703,7 @@ def get_comperative_schedule_data(request, cs_id):
         print("role id:", role.id)
         user_ace_role_ = Roles.objects.filter(id=role.id).first() if role.id else None
         print("role application:", user_ace_role_.application)
-        if user_ace_role_.application == "comparative_schedules":
+        if user_ace_role_.application == "comparative_schedule":
             user_comparative_schedule_role = user_ace_role_
             
     cs = ComparativeSchedules.objects.filter(cs_id=cs_id).first()
