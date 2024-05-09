4c195a47cb8f60f724c9e366f0bb485603b2bca4
vaChinaka <perseychinaka@gmail.com>
2024-05-09 09:38:03 +0200

    aa

--- beii_v1/settings.py
+++ beii_v1/settings.py
@@ -236,10 +236,10 @@ DATABASES = {
     #     'PASSWORD': 'Z3tdc_it@2021_',
     #     'HOST': 'localhost'
     # }
-  'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+#   'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 }
 
 # Password validation
