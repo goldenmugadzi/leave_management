55665490d7c082ff884cd3b2f0445607ee3c37d6
zehregit <zetdcgithub@gmail.com>
2024-05-14 16:49:02 +0200

    ...

--- reports/models.py
+++ reports/models.py
@@ -4,14 +4,14 @@ from it.users.models import *
 # Create your models here.
 class Report(models.Model):
     uploaded_by = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING)
-    region = models.ForeignKey(Regions, on_delete=models.DO_NOTHING)
-    section = models.ForeignKey(Sections, on_delete=models.DO_NOTHING)
+    region = models.CharField(max_length=100, blank=True, null=True)
     report_period = models.CharField(max_length=100, blank=True, null=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
+    date_created = models.CharField(max_length=100, blank=True, null=True)
+    date_updated = models.CharField(max_length=100, blank=True, null=True)
+    section = models.CharField(max_length=100, blank=True, null=True)
     file_name = models.CharField(max_length=100, blank=True, null=True)
+    file_type = models.CharField(max_length=100, blank=True, null=True)
     file_path = models.CharField(max_length=300, blank=True, null=True)
-    created_by = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='created_by')
 
     def __str__(self):
         return self.file_name
