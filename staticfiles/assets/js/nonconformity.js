216601801dc4e5ef30e8c971a807cad96aec8a62
zehregit <zetdcgithub@gmail.com>
2024-05-21 08:36:07 +0200

    ...

--- staticfiles/assets/js/nonconformity.js
+++
@@ -1,8 +0,0 @@
-$(document).on('change', '#id_status', function() {
-  var selectedValue = $(this).val();
-  if (selectedValue === 'False') {
-    $('#AcceptedForm').hide();
-  } else {
-    $('#AcceptedForm').show();
-  }
-});
