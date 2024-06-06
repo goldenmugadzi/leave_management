a70090bf1c4ed3ef50c7fb7f80bf0288d3e0b122
TatendaKwaramba <33118227+TatendaKwaramba@users.noreply.github.com>
2024-06-06 11:21:46 +0200

    ...

--- tokens/views.py
+++ tokens/views.py
@@ -422,9 +422,10 @@ def process_file(file_path):
 def cost_centers(request):return render(request, "tokens/cost_centers.html", {"cost_centers": CostCenter.objects.all()})
 def upload_centers(request):
     CostCenter.objects.all().delete()
-    file_path = "tokens\\cc.txt"
+    file_path = "tokens/"
+    filepath = os.path.join(file_path, 'cc.txt')
     try:
-        process_file(file_path)
+        process_file(filepath)
     except FileNotFoundError as e:
         print(e)
     return redirect("tokens:cost_centers")
