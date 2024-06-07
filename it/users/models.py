42417f87a4295dcf195c3b4c0bd89d98f2012bc1
TatendaKwaramba <33118227+TatendaKwaramba@users.noreply.github.com>
2024-06-07 11:55:58 +0200

    ...

--- it/users/models.py
+++ it/users/models.py
@@ -126,9 +126,11 @@ class UserProfile(AbstractUser):
 class Notification(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
     url = models.CharField(max_length=250)
+    notification_type = models.CharField(max_length=100, blank=True, null=True)
+    notification_id = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.message
@@ -161,6 +163,9 @@ class CostCenter(models.Model):
     code = models.CharField( max_length=20,unique=True)
     name = models.CharField(max_length=100, blank=True, null=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        ordering = ['id']
 
     def __str__(self):
         return self.id
