36c555ffe792ef5cbd34f7a2ebb2cef9aaacb464
TatendaKwaramba <33118227+TatendaKwaramba@users.noreply.github.com>
2024-06-18 17:01:05 +0200

    ...

--- competence_building/forms.py
+++ competence_building/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from .models import Document
+from .models import Subcategory
 
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = "__all__" #['category', 'name', 'region', 'section', 'file', 'created_by']
-        exclude=['created_by']
+        exclude=['created_by', 'section']
    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -15,11 +16,17 @@ class DocumentForm(forms.ModelForm):
                 'class': "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
             })
 
+
+class Subcategory(forms.ModelForm):
+  class Meta:
+    model = Subcategory
+    fields = ['category', 'name']
+
 class editDocumentForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = "__all__" #['category', 'name', 'region', 'section', 'file', 'created_by']
-        exclude=['created_by','file']
+        exclude=['created_by','file', 'section']
    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -33,7 +40,7 @@ class archiveDocumentForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = "__all__" #['category', 'name', 'region', 'section', 'file', 'created_by']
-        exclude=['created_by','']
+        exclude=['created_by','section']
    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- competence_building/models.py
+++ competence_building/models.py
@@ -20,15 +20,36 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Subcategory(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='subtype')
+    # section = models.ForeignKey(Sections, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    
+    def __str__(self):
+        return self.name 
+
 class Document(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, blank = True, null = True)
     region = models.ForeignKey(Regions, on_delete=models.CASCADE)
-    section = models.ForeignKey(Sections, on_delete=models.CASCADE)
     archive = models.BooleanField(default=False)
     file = models.FileField()
     created_by = models.ForeignKey(UserProfile, on_delete = models.DO_NOTHING, blank = True, null = True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Vacancies(models.Model):
+    region = models.ForeignKey(Regions, on_delete=models.CASCADE)
+    department = models.ForeignKey(Sections, on_delete=models.CASCADE)
+    archive = models.BooleanField(default=False)
+    file = models.FileField()
+    name = models.CharField(max_length=100, blank = True, null = True)
+    uploaded_by = models.ForeignKey(UserProfile, on_delete = models.DO_NOTHING, blank = True, null = True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
