0c8b0dfb75d2c519a8192472cb1f38b45c7e1478
TatendaKwaramba <33118227+TatendaKwaramba@users.noreply.github.com>
2024-06-27 10:36:16 +0200

    env

--- beii_v1/settings.py
+++ beii_v1/settings.py
@@ -32,18 +32,15 @@ SECRET_KEY = 'django-insecure-7per#nouy422m0!hn0!ecb7ltnq#!^#g!2r5&%^5c%v(!ivv&a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '172.16.8.20', '127.0.0.1', '172.16.8.99', '172.16.10.76']
+ALLOWED_HOSTS = [config('HOST')]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "http://172.16.8.20:9300"
-    'http://172.16.8.99:9300',
+    "http://"+config('HOST')+":"+config('PORT'),
+    "http://"+config('HOST')+":3000",
 ]
 
 # CORS_ALLOW_ALL_ORIGINS = True
-
-CSRF_TRUSTED_ORIGINS = ['http://172.16.8.20', 'http://172.16.8.20:9300', 'http://172.16.8.99:9300', "http://localhost:8000",]
+CSRF_TRUSTED_ORIGINS = ["http://"+config('HOST'), "http://"+config('HOST')+":"+config('PORT')]
 
 CORS_ALLOW_HEADERS = ('content-disposition', 'accept-encoding',
                       'content-type', 'accept', 'origin', 'authorization')
