5a054c9edcbbe049903add5091ba525c93d942e3
vachinaka <perseychinaka@gmail.com>
2024-07-12 13:43:31 +0200

    sdf

--- finance/purchase_request/forms.py
+++ finance/purchase_request/forms.py
@@ -46,8 +46,6 @@ class PurchaseRequestUpdateForm(forms.ModelForm):
                          "ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 "
                          "sm:text-sm sm:leading-6",
             })
-            if (field_name == 'procurement_plan_reference') or ( field_name == 'section'):
-                field.widget.attrs.update({'class': "select2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",})
             if isinstance(field.widget, forms.Textarea):
                 field.widget.attrs.update({'rows': '3'})
 
