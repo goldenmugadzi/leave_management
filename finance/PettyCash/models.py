f264012a270866805a83caa7663ae647b977e8ec
ashley mugwambi <70527457+ashiemugwa1997@users.noreply.github.com>
2024-10-16 16:10:04 +0200

    petty fix

--- finance/PettyCash/models.py
+++ finance/PettyCash/models.py
@@ -38,8 +38,8 @@ class Pettycash(models.Model):
     amount_disbursed = models.FloatField(blank=True, null=True)
     receipt_file = models.FileField(upload_to='uploads/pettycash', blank=True, null=True)
     amount_used = models.FloatField(blank=True, null=True)
-    payee = models.CharField(max_length=100, blank=True, null=True)
-    reason_for_supplier = models.CharField(max_length=100, blank=True, null=True)
+    # payee = models.CharField(max_length=100, blank=True, null=True)
+    # reason_for_supplier = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.petty_id
