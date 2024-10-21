e8e35540ff5baf7d91ba90799e42b08d00a00f5b
TatendaKwaramba <33118227+TatendaKwaramba@users.noreply.github.com>
2024-10-21 08:28:19 +0200

    ...

--- finance/comparative_schedules/views.py
+++ finance/comparative_schedules/views.py
@@ -1201,6 +1201,19 @@ def get_all_schedules(request):
     return render(request, user_page, { "fm_role": fm_role, "gm_role": gm_role, "procurement_role": procurement_role, "page_title": "RFQ Comparative Schedules"})
 
 @login_required
+def reports_all_schedules(request):
+    
+    user_id = request.user.id
+    print("user name: ", request.user.username, request.user.id)
+    user_profile = UserProfile.objects.filter(id=user_id).first()
+    print("user: ", user_profile.username, user_profile.id)
+    fm_role, gm_role = False, False
+    fm_role, gm_role, procurement_role = getUserFMGMRoles(user_profile)
+    
+    user_page = 'finance/comparative_schedules/cs_reports.html'
+    return render(request, user_page, { "fm_role": fm_role, "gm_role": gm_role, "procurement_role": procurement_role, "page_title": "RFQ Comparative Schedules"})
+
+@login_required
 def get_pending_committee(request):
     
     user_id = request.user.id
