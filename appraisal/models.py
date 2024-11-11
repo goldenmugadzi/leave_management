22e7f8d65db662784a15f4124280f530769a7a53
John-Dembaremba <johndembaremba97@gmail.com>
2024-11-11 15:38:34 +0200

    added(AppraisalsModels): added experiences

--- appraisal/models.py
+++ appraisal/models.py
@@ -9,8 +9,28 @@ class Appraisal(TimeStamp):
     The model use an abstract model(TimeStamp) with created_date and updated_date fields.
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    experience = models.ManyToManyField("Experience", through="AppraisalExperience")
+    
     def __str__(self) -> str:
         return f"{self.user}"
     
 
+class Experience(TimeStamp):
+    """Base model for experiences used in Appraisal"""
+    name = models.CharField(max_length=255, unique=True, null=False)
+    
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class AppraisalExperience(TimeStamp):
+    appraisal = models.ForeignKey(Appraisal, on_delete=models.CASCADE, related_name="appraisal")
+    experience = models.ForeignKey(Experience, on_delete=models.CASCADE, related_name="experience")
+    
+    # additional experiences
+    years_of_experience = models.PositiveIntegerField(max_length=2)
+    months_of_experience = models.PositiveIntegerField(max_length=2)
+    
+    def __str__(self) -> str:
+        return f"{self.appraisal.user} - {self.experience}"
     
