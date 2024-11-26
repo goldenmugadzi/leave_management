30a2b52347d2702f5102b7d8463722cd6bf69f32
John-Dembaremba <johndembaremba97@gmail.com>
2024-11-26 15:10:14 +0200

    feat(PerformanceReviewModels): created PerformanceReview related models

---
+++ appraisal/models/performance_review.py
@@ -0,0 +1,30 @@
+from helpers.models import TimeStamp
+from django.db import models
+from .appraisal import Appraisal
+
+class QuarterChoices(models.IntegerChoices):
+    Q1 = 1, "1st"
+    Q2 = 2, "2nd"
+    Q3 = 3, "3rd"
+    Q4 = 4, "4th"
+    
+class PerformanceProgressStrength(TimeStamp):
+    name = models.TextField(null=False, blank=False)
+    
+    def __str__(self) -> str:
+        return f"Strength-{self.id}"
+
+class PerformanceProgressWeakness(TimeStamp):
+    name = models.TextField(null=False, blank=False)
+    
+    def __str__(self) -> str:
+        return f"Weakness-{self.id}"
+
+class PerformanceProgressReview(TimeStamp):
+    appraisal = models.ForeignKey(Appraisal, on_delete=models.CASCADE)
+    quarter = models.PositiveIntegerField(choices=QuarterChoices.choices)
+    strength = models.ManyToManyField(PerformanceProgressStrength)
+    areas_of_weakness = models.ManyToManyField(PerformanceProgressWeakness)
+    
+    def __str__(self) -> str:
+        return f"{self.appraisal.user} - {self.quarter}"
