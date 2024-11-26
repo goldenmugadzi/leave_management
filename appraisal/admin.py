e1e6bbf778f73da31e8964c521bd21ca082d3fe7
John-Dembaremba <johndembaremba97@gmail.com>
2024-11-26 16:16:21 +0200

    add(AppraisalAdmin): added Performance related models

--- appraisal/admin.py
+++ appraisal/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Appraisal, Experience, AppraisalExperience
+from .models import Appraisal, Experience, AppraisalExperience, PerformanceProgressWeakness, PerformanceProgressReview, PerformanceProgressStrength
 
 # Register your models here.
 
 admin.site.register(Appraisal)
 admin.site.register(Experience)
 admin.site.register(AppraisalExperience)
+
+admin.site.register(PerformanceProgressStrength)
+admin.site.register(PerformanceProgressWeakness)
+admin.site.register(PerformanceProgressReview)
