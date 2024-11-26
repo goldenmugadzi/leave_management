21351f43eacee1485cff536ca0c8726725fcb78c
John-Dembaremba <johndembaremba97@gmail.com>
2024-11-26 15:11:21 +0200

    add(AppraisalHelpers): added data module as data provider

---
+++ appraisal/helpers/data/strenght_weakness.py
@@ -0,0 +1,24 @@
+
+STRENGTHS = [
+    (1, "Member is results oriented"),
+    (2, "Member is highly focused on the set targets."),
+    (3, "The member is meeting set targets."),
+    (4, "Member is highly motivated"),
+    (5, "Member clearly appreciates the organization`s overall thrust."),
+    (6, "Member effectively utilizes the organization` resources."),
+    (7, "Member values team work."),
+    (8, "Member is highly knowledgeable about the job requirements"),
+    (9, "Member exhibits sound judgment."),
+]
+
+WEAKNESSES = [
+    (1, "Member needs to be results oriented"),
+    (2, "Member needs to delegate duties equitably."),
+    (3, "Member needs to focus more on set targets."),
+    (4, "Member should meet set targets."),
+    (5, "Member needs to understand the organization’s thrust."),
+    (6, "Member needs to effectively utilize the organization’s resources"),
+    (7, "Member needs to value team work"),
+    (8, "Member needs to be more knowledgeable about the job requirements"),
+    (9, "Member needs to make sound judgment"),
+]
