e15d78d6b2028dfcfe3556d7a3121b83b5ff731c
John-Dembaremba <johndembaremba97@gmail.com>
2024-12-05 11:00:47 +0200

    added(PerformanceUrls): added detail and create url to sync the performance

--- appraisal/urls.py
+++ appraisal/urls.py
@@ -3,6 +3,7 @@ from .view import (AppraisalCreateView,
                    ExperienceCreateView, 
                    AppraisalTemplateView, 
                    experience_list_api,
+                   PerformanceReviewsAppraisalTemplateView,
                    PerformanceReviewsTemplateView,
                    PerformanceReviewsApprovalView
                    )
@@ -15,8 +16,9 @@ urlpatterns = [
     path('experience/create/', ExperienceCreateView.as_view(), name='create_experience'),
     
     # ================= Performance urls =================================
-    path('performance/review/', PerformanceReviewsTemplateView.as_view(), name='performance_review_index'),
-    path('performance/review/<int:appraisal_id>/<int:quarter>', PerformanceReviewsApprovalView.as_view(), name='performance_review_detail'),
+    path('performance/review/', PerformanceReviewsAppraisalTemplateView.as_view(), name='performance_review_index'),
+    path('performance/review/<int:appraisal_id>', PerformanceReviewsTemplateView.as_view(), name='performance_review_detail'),
+    path('performance/review/<int:appraisal_id>/<int:quarter>', PerformanceReviewsApprovalView.as_view(), name='performance_review_create'),
     
     # ----- api ------
     path('api/experience-list/', experience_list_api, name='experience_list_api'),
