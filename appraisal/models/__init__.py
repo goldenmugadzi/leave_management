be5b87ca2c6af943967150723a5bb277ccc8ba4f
John-Dembaremba <johndembaremba97@gmail.com>
2024-12-11 16:23:53 +0200

    refactor(AppraisalModels): refactor to sync kra models

--- appraisal/models/__init__.py
+++ appraisal/models/__init__.py
@@ -1,3 +1,4 @@
 from .appraisal import Appraisal, AppraisalExperience, Experience
 from .performance_review import PerformanceProgressReview, PerformanceProgressStrength, PerformanceProgressWeakness
 from .training import JobCompetency, Competency, InterventionStrategy, TrainingAndDevelopment
+from .kra import KeyResultArea, Activity, Target, TargetScore
--- appraisal/models/helpers.py
+++ appraisal/models/helpers.py
@@ -1,7 +1,29 @@
 from django.db import models
+from helpers.models.timestamp import TimeStamp
+
 
 class QuarterChoices(models.IntegerChoices):
     Q1 = 1, "1st"
     Q2 = 2, "2nd"
     Q3 = 3, "3rd"
     Q4 = 4, "4th"
+
+class YearQuarter(TimeStamp):
+    """
+    Represents a quarter within a specific year.
+
+    This model stores information about a single quarter in a given year, 
+    providing the quarter number (1st to 4th) and the year itself. The `quarter` 
+    field is constrained to values 1, 2, 3, or 4, representing the four quarters 
+    in a year.
+
+    Attributes:
+        year (int): The year for which the quarter is defined.
+        quarter (int): The quarter of the year (1, 2, 3, or 4).
+
+    """
+    year = models.PositiveIntegerField()  # Explicitly store the year
+    quarter = models.PositiveIntegerField(choices=QuarterChoices.choices)
+
+    def __str__(self):
+        return f"Year {self.year} - Q{self.quarter}"
