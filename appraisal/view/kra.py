e61bd28b5f756be73887c9b5923f5d48a32d7748
John-Dembaremba <johndembaremba97@gmail.com>
2024-12-13 14:47:06 +0200

    added(KRATemplateView): added the list of kras

--- appraisal/view/kra.py
+++ appraisal/view/kra.py
@@ -1,10 +1,9 @@
-from typing import Any, Dict
-from django.forms import BaseModelForm
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from typing import Any, Dict, List
 from django.views.generic import TemplateView
+from ..models import KeyResultArea
 from ..forms import YearQuarterForm
+from ..repository.kra import KRARepository
+from ..services.kra import KRAService
 
 class KRATemplateView(TemplateView):
     template_name = 'appraisal/kra/index.html'
@@ -12,6 +11,13 @@ class KRATemplateView(TemplateView):
     def get_year_quarter_form(self)->Dict[str, YearQuarterForm]:
         form = YearQuarterForm(self.request.POST or None)
         data = {"year_quarter_form": form}
+        return data
+    
+    def get_all_kra(self, year, quarter)->Dict[str, List[KeyResultArea]]:
+        repo = KRARepository()
+        service_handler = KRAService(kra_repo=repo)
+        kra_queryset = service_handler.get_all_by_quarter_year_use_case(year_number=year, quarter_number=quarter)
+        data = {"kra_objects": kra_queryset}
         return data
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
@@ -22,6 +28,7 @@ class KRATemplateView(TemplateView):
         query_param_quarter = self.request.GET.get('quarter')
         
         if query_param_year is not None or query_param_quarter is not None:
-            print(query_param_year, query_param_quarter)
+            context.update(self.get_all_kra(year=query_param_year, quarter=query_param_quarter))
         return context
+    
     
