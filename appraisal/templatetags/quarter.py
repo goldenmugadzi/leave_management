42329b70d22724149bd01ac98eeda8f390bf10c6
John-Dembaremba <johndembaremba97@gmail.com>
2024-12-13 16:58:49 +0200

    added(KRATemplateView): added template tags

---
+++ appraisal/templatetags/quarter.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def quarter_name(quarter):
+    quarters = {
+        1: "First Quarter",
+        2: "Second Quarter",
+        3: "Third Quarter",
+        4: "Fourth Quarter"
+    }
+    return quarters.get(int(quarter), "")
