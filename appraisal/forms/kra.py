a00a80242f248b1dc6924c0c641ee83b977fa281
John-Dembaremba <johndembaremba97@gmail.com>
2024-12-16 09:05:06 +0200

    added(KRAForms): created kra create form

--- appraisal/forms/kra.py
+++ appraisal/forms/kra.py
@@ -1,7 +1,12 @@
 from django import forms
-from ..models import YearQuarter
+from ..models import YearQuarter, KeyResultArea
 
 class YearQuarterForm(forms.ModelForm):
     class Meta:
         model = YearQuarter
         fields = ["year", "quarter"]
+
+class KraCreateForm(forms.ModelForm):
+    class Meta:
+        model = KeyResultArea
+        exclude = ["id", "created_date", "updated"]
