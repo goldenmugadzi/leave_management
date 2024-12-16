612aad76a8044fe9be6d83669de2032b1bc4cd63
John-Dembaremba <johndembaremba97@gmail.com>
2024-12-16 11:44:13 +0200

    fix(KRATypes): fixed weght_range validation

--- appraisal/helpers/types/kra.py
+++ appraisal/helpers/types/kra.py
@@ -7,8 +7,7 @@ class KRAType(BaseModel):
 
     @field_validator('weight')
     @classmethod
-    def check_weight_range(cls, v: float):
-        weight = v
-        if weight < 0 or weight > 100:
-            raise ValueError('Weight must be between 0 and 100.')
-        return v
+    def check_weight_range(cls, value: float) -> float:
+        if not 0 <= value <= 100:
+            raise ValueError("Weight must be between 0 and 100.")
+        return value
