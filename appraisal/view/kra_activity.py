d3f9c7be9f2680ddc2c0d825a3220b21884dde08
John-Dembaremba <johndembaremba97@gmail.com>
2024-12-17 11:19:47 +0200

    added(KRActivityTemplate): added kra object functionality

--- appraisal/view/kra_activity.py
+++ appraisal/view/kra_activity.py
@@ -14,4 +14,13 @@ from pydantic import ValidationError
 class KraActivityIndexTemplateView(TemplateView):
     template_name = 'appraisal/kra/activity/index.html'
     
+    def get_kra_object(self):
+        repo = KRARepository()
+        service_handler = KRAService(kra_repo=repo)
+        kra_obj_id = self.kwargs.get('kra_id')
+        return service_handler.get_kra_by_pk_use_case(kra_id=kra_obj_id)
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["kra_obj"] = self.get_kra_object()
+        return context
