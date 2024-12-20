f248d580166e1c55dd2bef9076ec8910dfd50f5b
John-Dembaremba <johndembaremba97@gmail.com>
2024-12-20 08:32:21 +0200

    added(TargetTypes): added type

--- appraisal/helpers/types/kra.py
+++ appraisal/helpers/types/kra.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel, Field, field_validator
+from typing import Literal
+from ...models.kra import METRIC_TYPES
 
 class KRAType(BaseModel):
     name: str = Field(..., description="The name of the KRA.")
@@ -11,3 +13,12 @@ class KRAType(BaseModel):
         if not 0 <= value <= 100:
             raise ValueError("Weight must be between 0 and 100.")
         return value
+
+MetricTypeValues = Literal[tuple(metric[1] for metric in METRIC_TYPES)]
+
+class TargetType(BaseModel):
+    name: str = Field(..., description="The name of the the Target.")
+    metric_type: MetricTypeValues = Field(..., description="The metric type of the Target.")
+    weight: float = Field(...,description="The weight of the Target.")
+    allowance_variance: float = Field(...,description="The allowance variance of the Target.")
+    target_value: float = Field(...,description="The target value of the Target.")
