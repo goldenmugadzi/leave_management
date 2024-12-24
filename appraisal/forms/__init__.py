60f478e320e1bcc4bba515fc34d57f72cbe6dfde
John-Dembaremba <johndembaremba97@gmail.com>
2024-12-24 11:25:32 +0200

    added(TargetForm): added target form

--- appraisal/forms/__init__.py
+++ appraisal/forms/__init__.py
@@ -2,5 +2,5 @@ from .appraisal import UserProfileForm, CostCenterForm, UserQualificationForm, A
 from .formsets import AppraisalExperienceFormset, UserQualificationFormset, InterventionStrategyFormSet, CompetencyFormSet
 from .performance_review import PerformanceReviewApprovalForm
 from .training import ActionsForm
-from .kra import YearQuarterForm,KraCreateForm, ActivityCreateForm
+from .kra import YearQuarterForm,KraCreateForm, ActivityCreateForm, TargetCreateForm
 
--- appraisal/forms/kra.py
+++ appraisal/forms/kra.py
@@ -1,5 +1,5 @@
 from django import forms
-from ..models import YearQuarter, KeyResultArea, Activity
+from ..models import YearQuarter, KeyResultArea, Activity, Target
 
 class YearQuarterForm(forms.ModelForm):
     class Meta:
@@ -15,3 +15,8 @@ class ActivityCreateForm(forms.ModelForm):
     class Meta:
         model = Activity
         exclude = ["id", "created_date", "updated", "kra"]
+        
+class TargetCreateForm(forms.ModelForm):
+    class Meta:
+        model = Target
+        exclude = ["id", "created_date", "updated", "activity"]
