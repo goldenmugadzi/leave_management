6de3bf67f011536656cfdf5d59a36257d67ed08a
ashley mugwambi <70527457+ashiemugwa1997@users.noreply.github.com>
2025-01-08 14:41:28 +0200

    calculation methods

--- appraisal/view/kra/targets.py
+++ appraisal/view/kra/targets.py
@@ -1,4 +1,3 @@
-
 from django.urls import reverse
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView, UpdateView
@@ -14,7 +13,7 @@ from pydantic import ValidationError
 
 class TargetsIndexView(TemplateView):
     template_name = 'appraisal/kra/targets/index.html'
-    
+
     @property
     def get_activity_object(self):
         repo = KraActivityRepository()
@@ -22,7 +21,7 @@ class TargetsIndexView(TemplateView):
         obj = service_handler.get_by_id_use_case(activity_id=self.kwargs.get('activity_id'))
         data = {"activity_object": obj}
         return data
-    
+
     @property
     def get_target_objects(self):
         repo = ActivityTargetRepository()
@@ -30,13 +29,14 @@ class TargetsIndexView(TemplateView):
         queryset = service_handler.fetch_by_activity_use_case(activity_id=self.kwargs.get('activity_id'))
         data = {"target_objects": queryset}
         return data
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(self.get_target_objects)
         context.update(self.get_activity_object)
         return context
-    
+
+
 class TargetCreateView(SuccessMessageMixin, CreateView):
     model = Target
     form_class = TargetCreateForm
@@ -51,31 +51,32 @@ class TargetCreateView(SuccessMessageMixin, CreateView):
         obj = service_handler.get_by_id_use_case(activity_id=self.kwargs.get('activity_id'))
         data = {"activity_object": obj}
         return data
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context[self.context_object_name] = context.get("form")
         context.update(self.get_activity_object)
         return context
-    
+
     def form_valid(self, form):
         try:
             payload = build_payload_target(request=self.request, form=form)
             activity_object = self.get_activity_object["activity_object"]
-            
+
             repo = ActivityTargetRepository()
             service_handler = TargetService(target_repository=repo)
             target_object = service_handler.create_use_case(activity_obj=activity_object,
-                                                              payload=payload)
+                                                            payload=payload)
             form.instance = target_object
         except Exception as e:
             messages.error(self.request, f"An unexpected error occurred, please try again")
             return self.form_invalid(form)
-        
+
         return super().form_valid(form)
-    
+
     def get_success_url(self) -> str:
         return reverse('target_index', kwargs={"activity_id": self.kwargs.get('activity_id')})
+
 
 class TargetUpdateView(SuccessMessageMixin, UpdateView):
     model = Target
@@ -83,14 +84,14 @@ class TargetUpdateView(SuccessMessageMixin, UpdateView):
     template_name = 'appraisal/kra/targets/create_update.html'
     success_message = 'Target updated successfully'
     context_object_name = "target_form"
-    
+
     @property
     def get_target_object(self):
         repo = ActivityTargetRepository()
         service_handler = TargetService(target_repository=repo)
         target_obj = service_handler.get_by_id_use_case(target_id=self.kwargs.get('target_id'))
         return target_obj
-    
+
     def get_object(self, queryset=None):
         """
         Override the default get_object method to retrieve the activity object using a custom service.
@@ -98,22 +99,21 @@ class TargetUpdateView(SuccessMessageMixin, UpdateView):
         obj = self.get_target_object
         return obj
 
-    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context[self.context_object_name] = context.get("form")
         context["activity_object"] = self.get_target_object.activity
         return context
-    
+
     def form_valid(self, form):
         try:
             payload = build_payload_target(request=self.request, form=form)
             target_object = self.get_target_object
-                 
+
             repo = ActivityTargetRepository()
             service_handler = TargetService(target_repository=repo)
             target_object = service_handler.update_use_case(target_obj=target_object,
-                                                              payload=payload)
+                                                            payload=payload)
             form.instance = target_object
         except ValidationError:
             return self.form_invalid(form)
@@ -121,45 +121,46 @@ class TargetUpdateView(SuccessMessageMixin, UpdateView):
             messages.error(self.request, f"An unexpected error occurred: {e}")
             return self.form_invalid(form)
         return super().form_valid(form)
-    
+
     def get_success_url(self) -> str:
         """
         Redirects to the index page after successful update.
         """
-        return reverse('target_update', kwargs={"activity_id": self.kwargs.get('activity_id'), "target_id": self.kwargs.get('target_id')})
-    
-    
+        return reverse('target_update', kwargs={"activity_id": self.kwargs.get('activity_id'),
+                                                "target_id": self.kwargs.get('target_id')})
+
+
 class TargetScoreUpdateView(SuccessMessageMixin, UpdateView):
     model = TargetScore
     form_class = TargetScoreForm
     template_name = 'appraisal/kra/targets/score_form.html'
     success_message = 'Scoring was set successfully'
     context_object_name = "score_form"
-    
+
     @property
     def get_target_score_object(self):
         repo = TargetScoreRepository()
         service_handler = TargetScoreService(target_score_repository=repo)
         obj = service_handler.get_by_target_id_use_case(target_id=self.kwargs.get('target_id'))
         return obj
-    
+
     def get_object(self, queryset=None):
         """
         Override the default get_object method to retrieve the activity object using a custom service.
         """
         obj = self.get_target_score_object
         return obj
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context[self.context_object_name] = context.get("form")
         context["target_score_object"] = self.get_target_score_object
         return context
-    
+
     def form_valid(self, form):
         try:
             payload = build_payload_score(request=self.request, form=form)
-                
+
             repo = TargetScoreRepository()
             service_handler = TargetScoreService(target_score_repository=repo)
             target_score_object = service_handler.update_use_case(target_score_obj=self.get_object(), data=payload)
@@ -170,10 +171,36 @@ class TargetScoreUpdateView(SuccessMessageMixin, UpdateView):
             messages.error(self.request, f"An unexpected error occurred: {e}")
             return self.form_invalid(form)
         return super().form_valid(form)
-    
+
     def get_success_url(self) -> str:
         """
         Redirects to the index page after successful update.
         """
-        return reverse('score', kwargs={"activity_id": self.kwargs.get('activity_id'), "target_id": self.kwargs.get('target_id')})
-    
+        return reverse('score', kwargs={"activity_id": self.kwargs.get('activity_id'),
+                                        "target_id": self.kwargs.get('target_id')})
+
+    def get_ActualVariance(actual, target):
+        actual_variance = actual - target
+        return actual_variance
+
+    def getRating(actual_variance, within_condition):
+        rating = 0
+        if actual_variance >= 0 and within_condition <= 1:
+            rating = 5
+        elif actual_variance > 0 and within_condition > 1:
+            rating = 6
+        elif actual_variance < 0 and within_condition <= 1:
+            rating = 4
+        elif actual_variance < 0 and within_condition > 1 and within_condition <= 2:
+            rating = 3
+        elif actual_variance < 0 and 2 < within_condition <= 3:
+            rating = 2
+        elif actual_variance < 0 and within_condition > 3:
+            rating = 1
+        return rating
+
+# within condition is the ratio of actual_variance to target_score and the allowable variance
+
+   def get_within_condition(actual_variance, allowable_variance):
+        within_condition=actual_variance/allowable_variance
+        return within_condition
