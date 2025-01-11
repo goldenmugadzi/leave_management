d609bff39599b76d4e013d1a6de97d9c4c02a071
John-Dembaremba <johndembaremba97@gmail.com>
2025-01-11 12:31:23 +0200

    added(TargetTemplateTags): added rating template tags

---
+++ appraisal/templatetags/rating.py
@@ -0,0 +1,18 @@
+from django import template
+from ..services.kra import TargetScoreService
+from ..repository.kra import TargetScoreRepository
+from loguru import logger
+register = template.Library()
+
+@register.filter
+def get_target_rating(activity_id)->int:
+    if not isinstance(activity_id, int):
+        return 0
+    repo = TargetScoreRepository()
+    service_handler = TargetScoreService(target_score_repository=repo)
+    
+    try:
+        return service_handler.calculate_activity_score_use_case(activity_id=activity_id)
+    except Exception as e:
+        logger.error(e)
+        return 0
