6ceb9ac6b2ddc4f531a707527a2b6d39ee085e59
John-Dembaremba <johndembaremba97@gmail.com>
2025-01-16 14:33:24 +0200

    added(TemplateTags): created templatetag for activity average weighted score

--- appraisal/templatetags/kra_calculations.py
+++ appraisal/templatetags/kra_calculations.py
@@ -2,12 +2,15 @@ from django import template
 from ..services.kra import TargetScoreService
 from ..repository.kra import TargetScoreRepository
 from loguru import logger
+from loguru import logger
 register = template.Library()
 
 @register.filter
 def get_target_rating(target_id)->int:
     if not isinstance(target_id, int):
+        logger.error(f"Invalid type for target_id: Expected int, got {type(activity_id).__name__}")
         return 0
+    
     repo = TargetScoreRepository()
     service_handler = TargetScoreService(target_score_repository=repo)
     
@@ -16,4 +19,19 @@ def get_target_rating(target_id)->int:
     except Exception as e:
         logger.error(e)
         return 0
+    
+@register.filter
+def get_activity_average_weighted_score(activity_id)->float:
+    if not isinstance(activity_id, int):
+        logger.error(f"Invalid type for activity_id: Expected int, got {type(activity_id).__name__}")
+        return 0.0
+
+    repo = TargetScoreRepository()
+    service_handler = TargetScoreService(target_score_repository=repo)
+    
+    try:
+        return service_handler.calculate_average_weighted_score_per_activity(activity_id=activity_id)
+    except Exception as e:
+        logger.error(e)
+        return 0.0
     
