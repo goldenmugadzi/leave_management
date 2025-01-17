03a0b113342dca3e1864d75347f1d60be336014b
John-Dembaremba <johndembaremba97@gmail.com>
2025-01-17 16:41:01 +0200

    added(ActivityViews): syncing additional appraisal field

--- appraisal/view/kra/activity.py
+++ appraisal/view/kra/activity.py
@@ -17,61 +17,63 @@ def get_kra_object(kra_id: int)->KeyResultArea:
     repo = KRARepository()
     service_handler = KRAService(kra_repo=repo)
     return service_handler.get_kra_by_pk_use_case(kra_id=kra_id)
-    
+
 class KraActivityIndexTemplateView(TemplateView):
     template_name = 'appraisal/kra/activity/index.html'
-    
+
     def get_activity(self):
         repo = KraActivityRepository()
         service_handler = ActivityService(activity_repo=repo)
         queryset = service_handler.fetch_by_kra_id_use_case(kra_id=self.kwargs.get('kra_id'))
         data = {"activity_objects": queryset}
         return data
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         kra_obj_id = self.kwargs.get('kra_id')
         context.update(self.get_activity())
         context["kra_obj"] = get_kra_object(kra_id=kra_obj_id)
         return context
-    
+
 class KraActivityCreateView(SuccessMessageMixin, CreateView):
     model = Activity
     form_class = ActivityCreateForm
     template_name = 'appraisal/kra/activity/create_update.html'
     success_message = 'Activity created successfully'
     context_object_name = "activity_form"
-    
+
     @property
     def get_kra_object(self):
         kra_obj_id = self.kwargs.get('kra_id')
         return get_kra_object(kra_id=kra_obj_id)
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context[self.context_object_name] = context.get("form")
         context["kra_object"] = self.get_kra_object
         return context
-       
-    
+
+
     def form_valid(self, form):
         try:
             payload = build_payload(request=self.request, form=form)
             kra_object = self.get_kra_object
             assigned_user_object = form.cleaned_data.get('assigned_user')
-            
+            appraiser_object = form.cleaned_data.get('appraiser')
+
             repo = KraActivityRepository()
             service_handler = ActivityService(activity_repo=repo)
             activity_object = service_handler.create_use_case(kra_object=kra_object,
                                                               assigned_user=assigned_user_object,
+                                                              appraiser=appraiser_object,
                                                               data=payload)
             form.instance = activity_object
         except Exception:
             messages.error(self.request, f"An unexpected error occurred, please try again")
             return self.form_invalid(form)
-        
+
         return super().form_valid(form)
-    
+
     def get_success_url(self) -> str:
         return reverse('kra_activity_index', kwargs={"kra_id": self.kwargs.get('kra_id')})
 
@@ -81,7 +83,7 @@ class KraActivityUpdateView(SuccessMessageMixin, UpdateView):
     template_name = 'appraisal/kra/activity/create_update.html'
     success_message = 'Activity updated successfully'
     context_object_name = "activity_form"
-    
+
     @property
     def get_activity_object(self):
         repo = KraActivityRepository()
@@ -89,7 +91,7 @@ class KraActivityUpdateView(SuccessMessageMixin, UpdateView):
         activity_obj_id = self.kwargs.get('activity_id')
         activity_obj = service_handler.get_by_id_use_case(activity_id=activity_obj_id)
         return activity_obj
-    
+
     def get_object(self, queryset=None):
         """
         Override the default get_object method to retrieve the activity object using a custom service.
@@ -97,22 +99,23 @@ class KraActivityUpdateView(SuccessMessageMixin, UpdateView):
         obj = self.get_activity_object
         return obj
 
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context[self.context_object_name] = context.get("form")
         context["kra_object"] = self.get_activity_object.kra
         return context
-    
+
     def form_valid(self, form):
         try:
             payload = build_payload(request=self.request, form=form)
             activity_object = self.get_activity_object
             assigned_user_object = form.cleaned_data.get('assigned_user')
-            
+            appraiser_object = form.cleaned_data.get('appraiser')
+
             repo = KraActivityRepository()
             service_handler = ActivityService(activity_repo=repo)
-            activity_object = service_handler.update_use_case(activity_object=activity_object, assigned_user=assigned_user_object, data=payload)
+            activity_object = service_handler.update_use_case(activity_object=activity_object, assigned_user=assigned_user_object, appraiser=appraiser_object, data=payload)
             form.instance = activity_object
         except ValidationError:
             return self.form_invalid(form)
@@ -120,7 +123,7 @@ class KraActivityUpdateView(SuccessMessageMixin, UpdateView):
             messages.error(self.request, f"An unexpected error occurred: {e}")
             return self.form_invalid(form)
         return super().form_valid(form)
-    
+
     def get_success_url(self) -> str:
         """
         Redirects to the index page after successful update.
@@ -128,4 +131,3 @@ class KraActivityUpdateView(SuccessMessageMixin, UpdateView):
         kra_obj_id = self.kwargs.get("kra_id")
         activity_obj_id = self.kwargs.get("activity_id")
         return reverse('kra_activity_update', kwargs={"kra_id": kra_obj_id, "activity_id": activity_obj_id})
-    
