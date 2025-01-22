5606ab7aa06b541b8f62e71f83efcc4d1fd58a02
vachinaka <perseychinaka@gmail.com>
2025-01-22 15:11:30 +0200

    merged main_staged into coreect1

--- it/users/views.py
+++ it/users/views.py
@@ -327,7 +327,7 @@ def add_user(request):
 
 
 @login_required
-# @allowed_roles(['Administrator'], ['users'])
+@allowed_roles(['Administrator'], ['users'])
 def get_user_records(request):
     user_page = 'users/user_index.html'
     user_title = request.user.get_full_name()
@@ -934,10 +934,10 @@ def fetch_center_parents(cost_center):
                                                                                                        'parent')
                     filtered_centers += filtered_centers_4
     return filtered_centers
-
+ 
 
 @login_required
-# @allowed_roles(['administrator'], ['users'])
+# @allowed_roles(['Administrator'], ['users'])
 def get_filtered_districts(request, region_id):
     print("Region ID: ", region_id)
     districts = Districts.objects.filter(region_id=region_id).all()
@@ -946,7 +946,7 @@ def get_filtered_districts(request, region_id):
 
 
 @login_required
-# @allowed_roles(['administrator'], ['users'])
+# @allowed_roles(['Administrator'], ['users'])
 def get_filtered_depots(request, district_id):
     print("District ID: ", district_id)
     depots = Depots.objects.filter(district_id=district_id).all()
@@ -956,7 +956,7 @@ def get_filtered_depots(request, district_id):
 
 
 @login_required
-# @allowed_roles(['administrator'], ['users'])
+# @allowed_roles(['Administrator'], ['users'])
 def get_user_all_groups(request):
     if request.method == "GET":
         user_title = request.user.get_full_name()
