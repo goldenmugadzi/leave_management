637a6fbf37b1407a95db85f27d542983c592d10a
ashley mugwambi <70527457+ashiemugwa1997@users.noreply.github.com>
2025-02-20 17:22:00 +0200

    tab fix

--- ACE2/views.py
+++ ACE2/views.py
@@ -414,9 +414,11 @@ def ace_awaiting_my_action(request):
 
     if ace_role == "pass":
         # I want objects from 2024 upwards
+        print("pass sh")
 
         for ace in Ace2.objects.filter(section=section, date_created__year__gte=2025, region=region):
             process = ace.process
+            print("normal sh")
 
             if process.approval_set.exists():
                 last_approval = process.approval_set.last()
@@ -435,32 +437,34 @@ def ace_awaiting_my_action(request):
                 if process.approval_set.filter(approved="Rejected").exists():
                     aces_to_process.remove(ace)
 
-    if ace_role == "pass" and user_profile.designation.id == 65 and user_profile.region.id == 3:
-        # I want objects from 2024 upwards
+        if ace_role == "pass" and user_profile.designation.id == 65 and user_profile.region.id == 3:
+            # I want objects from 2024 upwards
+            print("northern sh")
 
-        for ace in Ace2.objects.filter(date_created__year__gte=2025, region=region):
-            process = ace.process
+            for ace in Ace2.objects.filter(date_created__year__gte=2025, region=region):
+                process = ace.process
 
-            if process.approval_set.exists():
-                last_approval = process.approval_set.last()
-                current_step = last_approval.step.step
-            else:
-                current_step = 0
+                if process.approval_set.exists():
+                    last_approval = process.approval_set.last()
+                    current_step = last_approval.step.step
+                else:
+                    current_step = 0
 
-            next_step = current_step + 1
+                next_step = current_step + 1
 
-            workflow = process.workflow
-            step = workflow.step_set.filter(step=next_step, approver__in=user_roles).first()
+                workflow = process.workflow
+                step = workflow.step_set.filter(step=next_step, approver__in=user_roles).first()
 
-            if step:
-                aces_to_process.append(ace)
-                # remove aces that have been rejected
-                if process.approval_set.filter(approved="Rejected").exists():
-                    aces_to_process.remove(ace)
+                if step:
+                    aces_to_process.append(ace)
+                    # remove aces that have been rejected
+                    if process.approval_set.filter(approved="Rejected").exists():
+                        aces_to_process.remove(ace)
 
     else:
-        for ace in Ace2.objects.filter(date_created__year__gte=2024, region=region):
+        for ace in Ace2.objects.filter(date_created__year__gte=2025, region=region):
             process = ace.process
+            print("not sh")
 
             if process.approval_set.exists():
                 last_approval = process.approval_set.last()
@@ -479,7 +483,7 @@ def ace_awaiting_my_action(request):
                 if process.approval_set.filter(approved="Rejected").exists():
                     aces_to_process.remove(ace)
 
-    print(aces_to_process)
+    # print(aces_to_process)
 
     return render(request, 'finance/ace2/view_all_aces.html', {'aces': aces_to_process,
                                                                'ace_role': ace_role,
