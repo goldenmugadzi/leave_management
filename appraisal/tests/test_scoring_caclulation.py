b9e1fce6321f38a17363c2c1db594210921031ba
John-Dembaremba <johndembaremba97@gmail.com>
2025-03-15 18:19:03 +0200

    add(RatingCalculater): added new method for rate calculation and tested, implementation to views pending

--- appraisal/tests/test_scoring_caclulation.py
+++ appraisal/tests/test_scoring_caclulation.py
@@ -1,90 +1,32 @@
 from unittest import TestCase
-from ..helpers.getters import get_actual_variance, get_within_condition, get_rating
+from ..helpers.getters.rating import RatingCalculation, ActivityRatingEnum
+from pydantic import BaseModel
+from loguru import logger
 
-class TestGetActualVariance(TestCase):
-    def test_actual_variance_success(self):
-        # Arrange
-        actual_score = 90
-        target_score = 100
-        want = -10
+class RatingCalculationTestModel(BaseModel):
+    name: str
+    agreed_target: int
+    allowable_variance: int
+    actual_score: int
+    want_rate: int
 
-        # Act
-        got = get_actual_variance(actual_score=actual_score, target_score=target_score)
 
-        # Assert
-        self.assertEqual(want, got)
+class TestRatingCalculation(TestCase):
+    def test_calculate_rating(self):
+        test_cases = [
+            RatingCalculationTestModel(name="nothing was done", agreed_target=35, allowable_variance=5, actual_score=0, want_rate=ActivityRatingEnum.nothing_was_done.value),
+            RatingCalculationTestModel(name="no target met and below variance", agreed_target=35, allowable_variance=5, actual_score=29, want_rate=ActivityRatingEnum.no_target_met_below_variance.value),
+            RatingCalculationTestModel(name="no target met and within variance", agreed_target=35, allowable_variance=5, actual_score=34, want_rate=ActivityRatingEnum.no_target_met_within_variance.value),
+            RatingCalculationTestModel(name="target met and variance met", agreed_target=35, allowable_variance=5, actual_score=35, want_rate=ActivityRatingEnum.target_met_variance_met.value),
+            RatingCalculationTestModel(name="target met and within variance", agreed_target=35, allowable_variance=5, actual_score=36, want_rate=ActivityRatingEnum.target_met_within_variance.value),
+            RatingCalculationTestModel(name="target met and above variance", agreed_target=35, allowable_variance=5, actual_score=41, want_rate=ActivityRatingEnum.target_met_above_variance.value),
+        ]
 
-    def test_actual_variance_zero(self):
-        # Arrange
-        actual_score = 100
-        target_score = 100
-        want = 0
+        rating_handler = RatingCalculation()
 
-        # Act
-        got = get_actual_variance(actual_score=actual_score, target_score=target_score)
+        for test_case in test_cases:
+            variance_range_classify = rating_handler.classify_variance_range(agreed_target=test_case.agreed_target, allowable_variance=test_case.allowable_variance, actual_score=test_case.actual_score)
+            is_target_met = rating_handler.is_target_met(agreed_target=test_case.agreed_target, actual_target=test_case.actual_score)
 
-        # Assert
-        self.assertEqual(want, got)
-
-    def test_actual_variance_both_scores_zero(self):
-        # Arrange
-        actual_score = 0
-        target_score = 0
-        want = 0
-
-        # Act
-        got = get_actual_variance(actual_score=actual_score, target_score=target_score)
-
-        # Assert
-        self.assertEqual(want, got)
-
-class TestGetWithInCondition(TestCase):
-    def test_success_calculation(self):
-        # Arrange
-        actual_variance = 5
-        allowed_variance = 10
-        want = 0.5
-        # Act
-        got = get_within_condition(actual_variance=actual_variance, allowable_variance=allowed_variance)
-
-        # Assert
-        self.assertEqual(want, got)
-
-    def test_allowance_variance_zero_error(self):
-        # Arrange
-        actual_variance = 5
-        allowed_variance = 0
-        expected_err = "Allowance variance cannot be zero"
-
-        # Act & Assert
-        with self.assertRaises(ValueError) as context:
-            get_within_condition(actual_variance=actual_variance, allowable_variance=allowed_variance)
-
-        self.assertEqual(str(context.exception), expected_err)
-
-class TestGetRating(TestCase):
-    def test_rating_5(self):
-        self.assertEqual(get_rating(0, 0), 5)
-        self.assertEqual(get_rating(-1, 0.5), 5)
-        self.assertEqual(get_rating(-1, -0.5), 5)
-
-    def test_rating_6(self):
-        self.assertEqual(get_rating(1, 2), 6)
-        self.assertEqual(get_rating(5, 3), 6)
-
-    def test_rating_4(self):
-        self.assertEqual(get_rating(-1, -0.5), 4)
-
-    def test_rating_3(self):
-        self.assertEqual(get_rating(-1, -1.5), 3)
-
-    def test_rating_2(self):
-        self.assertEqual(get_rating(-1, -2.5), 2)
-
-    def test_rating_1(self):
-        self.assertEqual(get_rating(-1, -5), 1)
-
-    def test_rating_0(self):
-        # Case where no condition is met
-        self.assertEqual(get_rating(1, 0), 0)
-        self.assertEqual(get_rating(0, 2), 0)
+            got_rate = rating_handler.calculate_rating(is_target_met=is_target_met, variance_range_classify=variance_range_classify)
+            self.assertEqual(got_rate, test_case.want_rate)
