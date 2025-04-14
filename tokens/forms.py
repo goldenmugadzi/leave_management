ec13d2d5d1f3e10873e8e72a7c15d8e2f11f2025
TatendaKwaramba <33118227+TatendaKwaramba@users.noreply.github.com>
2025-04-14 13:53:15 +0200

    ...

--- tokens/forms.py
+++ tokens/forms.py
@@ -55,7 +55,7 @@ class TokenForm(forms.ModelForm):
     # attachments = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}),required=False)
     additional_attachments = forms.FileField(
         required=False,
-        widget=forms.ClearableFileInput(attrs={'multiple': True}),
+        widget=forms.FileInput(attrs={'multiple': True}),
     )
     class Meta:
         model = Token
