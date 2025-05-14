41577337cbad5ef2c36111289d35a647434b8566
goldenmugadzi <115697994+goldenmugadzi@users.noreply.github.com>
2025-05-14 14:05:10 +0200

    assets

--- Asset_Register/forms.py
+++ Asset_Register/forms.py
@@ -5,7 +5,7 @@ class ZetdcAssetForm(forms.ModelForm):
     class Meta:
         model = ZetdcAssets
         fields = '__all__'
-        # exclude = ['process', 'ace', 'requested_by', 'region', 'section']
+        exclude = ['purchase_cost', 'date_purchased', 'warrant', 'supplier', 'updated_at','created_at','created_by']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
