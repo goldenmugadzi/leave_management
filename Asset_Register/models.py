d46694ae1d6ab7525a6b9c369d3fd9d00f304b8a
goldenmugadzi <115697994+goldenmugadzi@users.noreply.github.com>
2025-05-15 09:47:01 +0200

    product

--- Asset_Register/models.py
+++ Asset_Register/models.py
@@ -24,13 +24,13 @@ class ZetdcAssets(models.Model):
     purchase_cost = models.DecimalField(max_digits=10, decimal_places=2)
     designation = models.ForeignKey(Designations, on_delete=models.DO_NOTHING , blank=True, null=True)
     department = models.ForeignKey(Sections, on_delete=models.DO_NOTHING, blank=True, null=True)
-    #date_purchased =models.DateField()
+    date_purchased =models.DateField()
     model = models.CharField(max_length=100,help_text="Asset Model",  choices=[('laptop', 'laptop') , ('desktop', 'desktop'),('printer', 'pinter'), ('router', 'router'),('switch', 'switch')])
-    #warrant = models.CharField(max_length=100, default='Unknown',blank=True, null=True)
+    warrant = models.CharField(max_length=100, default='Unknown',blank=True, null=True)
     cost_center = models.ForeignKey(CostCenter, on_delete=models.CASCADE, blank=True, null=True)
     created_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE, blank=True, null=True,related_name="creat")
     created_at = models.DateField(auto_now_add=True, blank=True, null=True)
-    #updated_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now=True)
     supplier = models.CharField(max_length=100, default='Unknown',blank=True, null=True)
 
 class HumanResource(models.Model):
