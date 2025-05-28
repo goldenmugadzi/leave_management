2c7c322db08b4f3ad6d79bdab883bdfb67c95906
John-Dembaremba <johndembaremba97@gmail.com>
2025-05-28 12:20:41 +0200

    fix(TargetSignal): fixed 'set appraisal scoring stage completed' to sync performance dimension logic

--- appraisal/signals/target.py
+++ appraisal/signals/target.py
@@ -14,17 +14,17 @@ def set_appraisal_scoring_stage_completed(sender, instance, created, **kwargs):
         pk = instance.id
         try:
             activity_object = instance.performance_dimension.activity
-            logger.info(f"[Approval Workflow stage] Scoring stage for Performance Dimension pk: {pk} handler initialized ...")
+            logger.info(f"[Approval Workflow stage] Scoring stage for Performance Dimension Target score pk: {pk} handler initialized ...")
             score_repo = TargetScoreRepository()
             score_qr = score_repo.fetch_by_appraisal_id(appraisal_id=activity_object.appraisal_kra.appraisal.id)
             
             if not score_qr.exists():
-                logger.error(f"[Approval Workflow stage] Scoring stage for Performance Dimension pk: {pk}, Scoring objects not found")
+                logger.error(f"[Approval Workflow stage] Scoring stage for Performance Dimension Target score pk: {pk}, Scoring objects not found")
                 return None
             
             unscored_activity_score_qr = score_qr.filter(is_scored=False)
             if unscored_activity_score_qr.exists():
-                logger.info(f"[Approval Workflow stage] Scoring stage for Performance Dimension pk: {pk}, Scoring objects not scored exists")
+                logger.info(f"[Approval Workflow stage] Scoring stage for Performance Dimension Target score pk: {pk}, Scoring objects not scored exists")
                 return None
             
             appraisal_object = activity_object.appraisal_kra.appraisal
@@ -32,18 +32,18 @@ def set_appraisal_scoring_stage_completed(sender, instance, created, **kwargs):
             workflow_qr = workflow_repo.retrieve_by_appraisal(appraisal_id=appraisal_object.id)
             
             if not workflow_qr.exists():
-                logger.error(f"[Approval Workflow stage] Scoring stage for Performance Dimension pk: {pk}, All stages not found")
+                logger.error(f"[Approval Workflow stage] Scoring stage for Performance Dimension Target score pk: {pk}, All stages not found")
                 return None            
             
             workflow_scoring_qr = workflow_qr.filter(stage_name=ApprovalStageData.scoring.value)
             if not workflow_scoring_qr.exists():
-                logger.error(f"[Approval Workflow stage] Scoring stage for Performance Dimension pk: {pk}, {ApprovalStageData.scoring.value} stage  not found")
+                logger.error(f"[Approval Workflow stage] Scoring stage for Performance Dimension Target score pk: {pk}, {ApprovalStageData.scoring.value} stage  not found")
                 return None
             
             workflow_repo.update(workflow_object=workflow_scoring_qr.first(), is_completed=True, updated_by=appraisal_object.appraiser)
             
-            logger.success(f"[Approval Workflow stage] Scoring stage for Performance Dimension pk: {pk} completed")
+            logger.success(f"[Approval Workflow stage] Scoring stage for Performance Dimension Target score pk: {pk} completed")
         except Exception as e:
-            logger.error(f"[Approval Workflow stage] Scoring stage for Performance Dimension pk: {pk}, failed with error: {e}")
+            logger.error(f"[Approval Workflow stage] Scoring stage for Performance Dimension Target score pk: {pk}, failed with error: {e}")
             return None
 
