a50c9c3f9486799267eabe57c707082ff58a1bc6
John-Dembaremba <johndembaremba97@gmail.com>
2025-07-10 11:10:02 +0200

    add(DepartmentObjectiveRepo): added update repo and tests

--- appraisal/repository/departmental_workplan.py
+++ appraisal/repository/departmental_workplan.py
@@ -13,3 +13,25 @@ class DepartmentalObjectiveRepository:
             )
         except Exception as e:
             raise Exception(f"DepartmentalObjectiveRepository Create Repo failed with error: {e}")
+        
+    def update(self, dep_objective_instance: DepartmentObjective, update_user_obj: UserProfile, key_result_area_obj: KeyResultArea, department_objective_desc: str)->DepartmentObjective:
+        try:
+            is_changed = False
+            
+            if dep_objective_instance.updated_by != update_user_obj:
+                dep_objective_instance.updated_by = update_user_obj
+                is_changed = True
+            
+            if dep_objective_instance.key_result_area != key_result_area_obj:
+                dep_objective_instance.key_result_area = key_result_area_obj
+                is_changed = True
+                
+            if dep_objective_instance.objective_description != department_objective_desc:
+                dep_objective_instance.objective_description = department_objective_desc
+                is_changed = True
+            
+            if is_changed:
+                dep_objective_instance.save()
+            return dep_objective_instance
+        except Exception as e:
+            raise Exception(f"DepartmentalObjectiveRepository Update repo with pk: {dep_objective_instance.id}, failed with error: {e}")
--- appraisal/tests/repository/test_departmental_workplan.py
+++ appraisal/tests/repository/test_departmental_workplan.py
@@ -59,3 +59,85 @@ class TestDepartmentalObjectiveCreateRepository(TestCase):
                 )
                 
             self.assertIn(f"DepartmentalObjectiveRepository Create Repo failed with error: {exception_desc}", str(context.exception))
+            
+class TestDepartmentalObjectiveUpdateRepository(TestCase):
+    def setUp(self):
+        self.repo = DepartmentalObjectiveRepository()
+        self.mock_updater_user = Mock(spec=UserProfile)
+        self.mock_kra_obj = Mock(spec=KeyResultArea)
+        self.mock_objective_desc = "Department Objective"
+        
+    def mock_department_objective_instance(self):
+        mock = Mock(spec=DepartmentObjective)
+        mock.updated_by = self.mock_updater_user
+        mock.key_result_area = self.mock_kra_obj
+        mock.objective_description = self.mock_objective_desc
+        return mock
+        
+    def test_update_orm_called_once_for_changes(self):
+        mock_save_orm = Mock()
+        mock_department_objective_instance = self.mock_department_objective_instance()
+        changed_objective_description = "Changed Description"
+        mock_department_objective_instance.save = mock_save_orm
+        
+        self.repo.update(
+            dep_objective_instance=mock_department_objective_instance,
+            update_user_obj=self.mock_updater_user,
+            key_result_area_obj=self.mock_kra_obj,
+            department_objective_desc=changed_objective_description
+        )
+        
+        mock_department_objective_instance.save.assert_called_once()
+    
+    def test_update_orm_not_called_no_changes(self):
+        mock_save_orm = Mock()
+        mock_department_objective_instance = self.mock_department_objective_instance()
+        mock_department_objective_instance.save = mock_save_orm
+        
+        self.repo.update(
+            dep_objective_instance=mock_department_objective_instance,
+            update_user_obj=self.mock_updater_user,
+            key_result_area_obj=self.mock_kra_obj,
+            department_objective_desc=self.mock_objective_desc
+        )
+        
+        mock_department_objective_instance.save.assert_not_called()
+        
+    def test_update_successfully(self):
+        mock_save_orm = Mock()
+        mock_department_objective_instance = self.mock_department_objective_instance()
+        changed_objective_description = "Changed Description"
+        mock_department_objective_instance.save = mock_save_orm
+        
+        got = self.repo.update(
+            dep_objective_instance=mock_department_objective_instance,
+            update_user_obj=self.mock_updater_user,
+            key_result_area_obj=self.mock_kra_obj,
+            department_objective_desc=changed_objective_description
+        )
+        
+        mock_department_objective_instance.objective_description = changed_objective_description
+        self.assertEqual(got, mock_department_objective_instance)
+        
+    def test_update_error(self):
+        mock_save_orm = Mock()
+        mock_department_objective_instance = self.mock_department_objective_instance()
+        mock_department_objective_instance.id = 1
+        exception_desc = "Database error"
+        changed_objective_description = "Changed Description"
+
+        mock_department_objective_instance.save= mock_save_orm
+        mock_department_objective_instance.save.side_effect = Exception(exception_desc)
+        
+        with self.assertRaises(Exception) as context:
+            
+            self.repo.update(
+                dep_objective_instance=mock_department_objective_instance,
+                update_user_obj=self.mock_updater_user,
+                key_result_area_obj=self.mock_kra_obj,
+                department_objective_desc=changed_objective_description
+            )
+            
+        self.assertIn(f"DepartmentalObjectiveRepository Update repo with pk: 1, failed with error: {exception_desc}", str(context.exception))
+
+
