077cc35408bf33e144020795f5923558db0e6aa2
John-Dembaremba <johndembaremba97@gmail.com>
2025-08-13 14:51:25 +0200

    add(CurrentQuarter): added CurrentQuater handler

--- appraisal/helpers/types/quarters.py
+++ appraisal/helpers/types/quarters.py
@@ -4,3 +4,9 @@ class ApprovedQuartersType(BaseModel):
     appraisal_kra_id: int
     quarter_name: str
     is_approved: bool
+    
+class CurrentQuartersType(BaseModel):
+    is_within_first_quarter: bool
+    is_within_second_quarter: bool
+    is_within_third_quarter: bool
+    is_within_fourth_quarter: bool
