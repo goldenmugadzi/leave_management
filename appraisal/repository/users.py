03f748a1b3c839a724a9c08ab64bdc05dd1cfa51
John-Dembaremba <johndembaremba97@gmail.com>
2025-08-19 15:04:53 +0200

    add(UserProfileService): added use cases for cost centers users

--- appraisal/repository/users.py
+++ appraisal/repository/users.py
@@ -5,11 +5,11 @@ from it.users.models import UserProfile
 class UserProfileRepository:
     def fetch_by_cost_center_pk(self, cost_center_id: int)->QuerySet[UserProfile]:
         try:
-            return UserProfile.objects.filter(cost_center__id=cost_center_id)
+            return UserProfile.objects.filter(cost_center__id=cost_center_id).select_related('cost_center')
         except Exception as e:
             raise Exception(f"[UserProfileRepository] fetch_by_cost_center_pk with cost_center_id: {cost_center_id}, failed with error: {e}")
         
-    def get_by_pk(self, user_id: int)->QuerySet[UserProfile]:
+    def get_by_pk(self, user_id: int)->UserProfile|None:
         try:
             qr = UserProfile.objects.filter(id=user_id)
             return qr.first()
