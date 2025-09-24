f93ff453cbf01f8e4269e03305c5b13d42e750dd
John-Dembaremba <johndembaremba97@gmail.com>
2025-09-24 10:31:43 +0200

    fix(Scores): Scores calculation fixed

--- appraisal/helpers/types/final_results.py
+++ appraisal/helpers/types/final_results.py
@@ -19,3 +19,4 @@ class FinalRatingType(BaseModel):
     total_score: Annotated[Decimal, Field(max_digits=5, decimal_places=2)] = Field(
         ..., description="Quarterly total score."
     )
+    
