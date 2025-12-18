ec75acf199718e19b60b931dc24da28f65cf899b
John-Dembaremba <johndembaremba97@gmail.com>
2025-12-18 16:13:01 +0200

    appraisal roles creation

--- appraisal/signals/kra.py
+++ appraisal/signals/kra.py
@@ -5,11 +5,19 @@ from ..helpers.types.kra import TargetScoreType
 from ..models.kra import APPRAISAL_KRA_REVIEWER_STATUS_CHOICES
 from loguru import logger
 from django.db import transaction
-from it.users.models import Application
-
+from it.users.models import Application, Roles
 
 def create_kra_roles_handler(sender, **kwargs):
     try:
+        logger.info(f"Create Appraisal App Roles")
+        app_obj, _ = Application.objects.get_or_create(name="Appraisal", defaults={"fullname": "Appraisal"})
+
+        roles = ["Section Head", "KRA Creator"]
+        for role in roles:
+            _, _ = Roles.objects.get_or_create(role=role, 
+                                            name=role, 
+                                            application=app_obj.name, 
+                                            defaults={"app_id": app_obj, "description": role})
         logger.success('KRA roles successfully created for all modules.')
     except Exception as e:
         logger.error(f"Creating Kra Roles handler failed with error: {e}")
