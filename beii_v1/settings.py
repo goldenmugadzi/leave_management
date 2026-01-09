bf55d6fb939433bb38f26534232d334e00cad7e4
VaChinaka <perseyChinaka@gmail.com>
2026-01-09 15:38:14 +0200

    logging

--- beii_v1/settings.py
+++ beii_v1/settings.py
@@ -373,6 +373,11 @@ LOGGING = {
             'level': 'DEBUG',
             'propagate': True,
         },
+        'it.users.schema': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
         'dashboard_enhancement': {
             'handlers': ['dashboard_file', 'console'],
             'level': 'INFO',
