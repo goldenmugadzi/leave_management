9fd203a77a878345f40263065cd346ddcb253d9a
goldenmugadzi <115697994+goldenmugadzi@users.noreply.github.com>
2026-01-09 12:05:57 +0200

    password

--- meetings/views.py
+++ meetings/views.py
@@ -275,6 +275,7 @@ def booked_venues_datatable(request):
    
 def get_exchange_account():
   ## changed mail password
+  ## password change
     from decouple import config as cnf
     print(cnf)
     credentials = Credentials(
